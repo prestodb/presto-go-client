db484337514ef3c90bf4ebbfb09809f9bc1fae0b
pejia <pejia@contextlogic.com>
2025-06-08 10:35:39 +0800

    add ExecContext support

--- presto/presto.go
+++ presto/presto.go
@@ -531,7 +531,111 @@ func (st *driverStmt) NumInput() int {
 }
 
 func (st *driverStmt) Exec(args []driver.Value) (driver.Result, error) {
-	return nil, ErrOperationNotSupported
+	return nil, driver.ErrSkip
+}
+
+func (st *driverStmt) ExecContext(ctx context.Context, args []driver.NamedValue) (driver.Result, error) {
+	query := st.query
+	var hs http.Header
+
+	if len(args) > 0 {
+		hs = make(http.Header)
+		var ss []string
+		for _, arg := range args {
+			s, err := Serial(arg.Value)
+			if err != nil {
+				return nil, err
+			}
+			if arg.Name == prestoUserHeader {
+				st.user = s
+				hs.Add(prestoUserHeader, st.user)
+			} else if arg.Name == prestoClientTagsHeader {
+				hs.Add(prestoClientTagsHeader, s)
+			} else if arg.Name == prestoClientInfoHeader {
+				hs.Add(prestoClientInfoHeader, s)
+			} else {
+				ss = append(ss, s)
+			}
+		}
+
+		if len(ss) > 0 {
+			if hs.Get(preparedStatementHeader) == "" {
+				hs.Add(preparedStatementHeader, preparedStatementName+"="+url.QueryEscape(st.query))
+			}
+			query = "EXECUTE " + preparedStatementName + " USING " + strings.Join(ss, ", ")
+		}
+	}
+
+	req, err := st.conn.newRequest("POST", st.conn.baseURL+"/v1/statement", strings.NewReader(query), hs)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := st.conn.roundTrip(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var sr stmtResponse
+	d := json.NewDecoder(resp.Body)
+	d.UseNumber()
+	err = d.Decode(&sr)
+	if err != nil {
+		return nil, fmt.Errorf("presto: %v", err)
+	}
+
+	err = handleResponseError(resp.StatusCode, sr.Error)
+	if err != nil {
+		return nil, err
+	}
+
+	// 等待查询完成
+	for {
+		if sr.NextURI == "" {
+			break
+		}
+
+		req, err = st.conn.newRequest("GET", sr.NextURI, nil, nil)
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err = st.conn.roundTrip(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+
+		d = json.NewDecoder(resp.Body)
+		d.UseNumber()
+		err = d.Decode(&sr)
+		resp.Body.Close()
+		if err != nil {
+			return nil, fmt.Errorf("presto: %v", err)
+		}
+
+		err = handleResponseError(resp.StatusCode, sr.Error)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &driverResult{
+		affectedRows: sr.Stats.ProcessedRows,
+	}, nil
+}
+
+// driverResult 实现了 driver.Result 接口
+type driverResult struct {
+	affectedRows int
+}
+
+func (r *driverResult) LastInsertId() (int64, error) {
+	return 0, ErrOperationNotSupported
+}
+
+func (r *driverResult) RowsAffected() (int64, error) {
+	return int64(r.affectedRows), nil
 }
 
 type stmtResponse struct {
